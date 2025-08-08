e925a6cd9ec02ab2287a33d07fd68c82705da4cd
smita20BCS4643 <smitash3011@gmail.com>
2025-08-08 17:35:16 +0530

    operations in js

---
+++ JavaScript/part1/operation.js
@@ -0,0 +1,33 @@
+let addition = 4 + 5
+
+let score = 102
+let bonus = 40
+
+let totaladdition = score + bonus
+
+let add = 4 + 5
+let mult = 4 * 5
+let sub = 6 - 3
+let div =  8 / 2
+let reminder = 8 % 3
+let expo =  2 ** 3
+
+console.log(add,mult,sub, div,reminder,expo)
+
+
+let myscore = 1220
+myscore++
+console.log(myscore)
+
+let urscore = 232
+urscore--
+console.log(urscore)
+
+
+// comparision operation 
+let num1 = 4
+let num2 = 5
+
+console.log(num1 == num2);
+console.log(num1 != num2);
+
