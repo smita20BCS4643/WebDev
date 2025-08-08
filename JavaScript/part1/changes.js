4d83c777ac8c3352d6a2ad04362ccd5ae3b03fa3
smita20BCS4643 <smitash3011@gmail.com>
2025-08-08 16:20:58 +0530

    Assiging new value to existing variable

---
+++ JavaScript/part1/changes.js
@@ -0,0 +1,16 @@
+let gameName = "spidername"
+ 
+gameName = "batman"
+
+console.log(gameName)
+
+
+// const - used to reserve space in the memory  this is keyword
+
+const username = "smitash3011" // this is unchangeble 
+
+// username = "new UserName" 
+// can not be assigned 
+
+
+
