a72941e12ce34103e1a8e497542ffac2cd1ce52c
smita20BCS4643 <smitash3011@gmail.com>
2025-08-20 11:06:12 +0530

    primitive datatype of js

---
+++ JavaScript/part2/primitives.js
@@ -0,0 +1,71 @@
+// //number
+
+// const { useDebugValue } = require("react");
+
+// let balance = 245;
+
+// let anotherbalnace = new Number(123);
+
+// console.log(typeof(balance));
+// console.log(typeof(anotherbalnace));
+
+
+// console.log(anotherbalnace);
+// console.log(balance);
+
+
+// //boolean
+// let isActive = true;
+// let isrealActive = false;
+
+// console.log(typeof(isActive));
+// console.log(typeof(isrealActive)); // not recommanded
+
+
+// let firstname;
+// console.log((firstname));
+// // it means it exist but doesn't have type 
+
+
+// let lastname = null;
+// console.log((lastname)); 
+// //it will give null
+
+
+// let middlename= undefined;
+// console.log((middlename));
+// // we explicitly defined its undefined...it means it will come out in the future but not now
+
+// //  means it not defined it.
+
+
+
+
+// string
+
+// let mystring = "hello namaste";
+// let mystring1 = 'hola';
+// let username = 'hiteafff';
+
+// let oldGreet = mystring + "hello";
+// console.log((oldGreet));
+
+
+
+
+
+// let greetmsg = `heehee ${username} !`;
+
+// console.log((mystring));
+// console.log((mystring1));
+// console.log((username));
+// console.log((greetmsg));
+
+
+
+// symbol 
+let sm1 = Symbol();
+let sm2 = Symbol();
+console.log(sm1 == sm2);
+
+// internally they store a different values and unique values
