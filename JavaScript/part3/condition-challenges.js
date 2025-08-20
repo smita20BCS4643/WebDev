82df47b1bc63fe618643dede0c1ab65c27a692ef
smita20BCS4643 <smitash3011@gmail.com>
2025-08-20 19:30:58 +0530

    5 Js Condition challenges

---
+++ JavaScript/part3/condition-challenges.js
@@ -0,0 +1,97 @@
+// check if a number is greater than another number
+
+let num1 = 10;
+let num2 = 20;
+if(num1> num2)
+{
+    console.log(("{num1} is greater than {num2}"));
+    
+}
+else
+{
+    console.log(("{num1} is not greater than {num2}"));
+    
+}
+
+
+let username = 'user1';
+let anotherusrr = 'user1';
+
+if(username == anotherusrr)
+{
+    console.log("pick another username");
+    
+}
+else
+{
+    console.log("Good to go with this username");
+    
+}
+
+
+//checking if a variablw is a number or not
+
+let score = 44
+if(typeof score === "number")
+{
+   console.log("yep, this  is a number");
+   
+}
+else{
+    console.log("No that is not a number");
+    
+}
+
+
+let score2 = "44"
+if(typeof score2 === "number")
+{
+   console.log("yep, this  is a number");
+   
+}
+else{
+    console.log("No that is not a number");
+    
+}
+
+
+let score3 = []
+if(typeof score3 === "array")
+{
+   console.log("yep, this  is a number");
+   
+}
+else{
+    console.log("No that is not a number");
+    
+}
+
+// check if a boolean value is true or false
+
+let isTeaReady = false;
+
+if(isTeaReady)
+{
+    console.log("tea is ready");
+    
+}
+else
+{
+    console.log("Not reeady");
+    
+}
+
+//check the array empty or not
+
+ let items = [];
+
+ if(items.length === 0)
+ {
+    console.log("array is empty");
+
+ }
+ else
+ {
+    console.log("array is not empty");
+    
+ }
