7474ee0c6ceafd8974d582ca026cd993f741cd46
smita20BCS4643 <smitash3011@gmail.com>
2025-08-21 14:28:34 +0530

    loops in js

---
+++ JavaScript/part5/loop.js
@@ -0,0 +1,36 @@
+/*
+ write a while loop that calculates the sum of all numbers from 1 to 5 and stores the result in varibale named sum
+
+
+*/
+let i = 1;
+let sum =0;
+ while (i<=10) {
+    sum += i;
+    i++;
+ }
+ console.log(sum," ");
+
+
+ /*
+ write a while loop that counts down form 5 to 1 and stores the numbers in array named CountDown
+ */
+let CountDown = [];
+
+while(i!=0)
+{
+    CountDown.push(i);
+    console.log(i);
+    i--;
+    
+} 
+/*
+ 3. write a do whule loop that prompts a user to enter their favorite tea type until they enter stop
+ store each tea type in an aray named "Tea Collection"
+*/
+
+do {
+    
+} while (condition);
+
+
