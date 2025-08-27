53a3fa1b3c1cf71d956ca696c0594cdbf2ae06b2
smita20BCS4643 <smitash3011@gmail.com>
2025-08-27 16:49:28 +0530

    levelUp challenge js

--- JavaScript/part5/levelup_challenges.js
+++ JavaScript/part5/levelup_challenges.js
@@ -37,8 +37,83 @@ for(let i = 0; i<=cities.length; i++)
     if(cities[i] !== "Paris")
     {
         visitedCities.push(cities[i]);
+        continue;
+        
     }
 
 }
 
 console.log(visitedCities);
+
+
+/*
+ user a for of loop to iterate through the array [ 1, 2, 3, 4 ,5] and stop when 4 is found.
+ store the numbers before 4 in an arrya named SmallNumbers
+
+*/
+let numbers = [1,3,2,4,5]
+let SmallNumbers = []
+
+for (const num of numbers) {
+    // console.log(num);
+    if(num != 4)
+    {
+       SmallNumbers.push(num);
+     continue;
+    }
+    console.log(SmallNumbers);
+    
+    
+}
+
+/*
+4. use a for of loop to itreate through the array [chai, green tea, hearbal tea, black tea]
+ and skip herbal tea and store in another array
+
+ */
+
+ let chaiarray =["green tea" , "chai", "herbal tea", "black tea"];
+ let newchai =[];
+
+ for (const eachchai of chaiarray) {
+    if(eachchai == "herbal tea")
+    {
+       continue;
+    }
+    newchai.push(eachchai);
+    
+ }
+ console.log(newchai);
+
+ 
+ 
+
+/*
+5. use for in loop to loop through an object containing city populations.
+stop the loop when the population of berline is found and store all the previous populations in a new object named
+cityPopulations
+
+let citiesPopulation = {
+"london" : 89909898,
+"new york" : 8409043,
+"paris": 22556544,
+"berlin": 342342323
+};
+
+*/
+
+// let citiesPopulation = {
+// "london" : 89909898,
+// "new york" : 8409043,
+// "paris": 22556544,
+// "berlin": 342342323
+// };
+
+// let cityPopulations = []
+
+// for (const key in object) {
+//     if (Object.prototype.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
