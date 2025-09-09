2f95fef82dd0f9c8e1cdc47aba73816867edadee
smita20BCS4643 <smitash3011@gmail.com>
2025-09-09 17:36:56 +0530

    the inner function will retain the property of outer funtion insider it

--- JavaScript/part9_adv/clouser.js
+++ JavaScript/part9_adv/clouser.js
@@ -15,3 +15,9 @@ let inner = outer();
 console.log(inner());
 console.log(inner());
 console.log(inner());
+
+
+// when we calling the function we are calling the inner function which is return function one
+
+// but when we see the output it is 2 ,3 , 4 
+// means it regaining / retaining the memory of function
