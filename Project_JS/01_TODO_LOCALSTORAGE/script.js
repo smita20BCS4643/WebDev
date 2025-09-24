f7241fcf704a2e8d9485f9046149f98a67308159
smita20BCS4643 <smitash3011@gmail.com>
2025-09-24 22:36:17 +0530

    Script.js

---
+++ Project_JS/01_TODO_LOCALSTORAGE/script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks = JSON.parse(localStorage.getItem("task")) ||[];
+
+tasks.forEach(task => renderTask(task));
+
+
+addTaskButton.addEventListener('click', () =>
+{
+   const taskText = todoInput.value.trim()
+   if(taskText ==="") return;
+
+   const newTask = {
+    id:Date.now(),
+    text:taskText,
+    completed: false 
+   }
+
+   tasks.push(newTask);
+   renderTask(newTask);
+   SaveTask();
+   todoInput.value = " "; // clear input
+   console.log(tasks);
+})
+
+function renderTask(task)
+{
+   const li = document.createElement('li');
+   li.setAttribute("data-id", task.id);
+   if(task.completed) li.classList.add("completed");
+   li.innerHTML = `
+   <span>${task.text}</span>
+   <button>delete</button>`;
+
+    li.addEventListener("click", (e)=>{
+      if(e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      SaveTask();
+    })
+
+    li.querySelector("button").addEventListener("click", (e)=>{
+      e.stopPropagation(); //prevent toggle from firing or bubling
+      tasks = tasks.filter((t) => t.id === tasks.id);
+      li.remove();
+      SaveTask();
+    })
+
+   todoList.appendChild(li);
+
+   console.log(task);
+   
+}
+
+function SaveTask()
+{
+   localStorage.setItem("task", JSON.stringify(tasks));
+}
+})
