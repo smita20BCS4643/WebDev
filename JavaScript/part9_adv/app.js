bcd01f2b83c6acfa7ead88ea6d52add85d1baf95
smita20BCS4643 <smitash3011@gmail.com>
2025-09-24 22:33:47 +0530

    modern and old js ES^

---
+++ JavaScript/part9_adv/app.js
@@ -0,0 +1,4 @@
+//default import
+
+const mathOperations = require("./mathOperationsC.js")
+console.log(mathOperations.add(1033, 42));
---
+++ JavaScript/part9_adv/appM.js
@@ -0,0 +1,10 @@
+// Module import file
+
+//default import
+import multiply from "./mathOperationM.js";
+
+//named import
+import { addition, sub } from "./mathOperationM.js";
+
+console.log(sub(2, 2));
+console.log(addition(2, 2));
